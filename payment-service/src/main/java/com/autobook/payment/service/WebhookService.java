9cd2e671ccf4b7aaa8c3d080a5d243d5074ea275
abdellahbardich <87817677+abdellahbardich@users.noreply.github.com>
2025-03-24 02:06:42 +0000

    moving out from aws

--- payment-service/src/main/java/com/autobook/payment/service/WebhookService.java
+++
@@ -1,8 +0,0 @@
-package com.autobook.payment.service;
-
-import com.autobook.payment.dto.WebhookEventDto;
-
-public interface WebhookService {
-    void processWebhookEvent(WebhookEventDto webhookEvent);
-    boolean validateWebhookSignature(String requestBody, String signature);
-}
